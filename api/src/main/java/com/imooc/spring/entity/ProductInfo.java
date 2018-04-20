e408d26612013114dc05dd4744eaad346e2dc635
zhangxishuo <zhangxishuo1998@gmail.com>
2018-04-20 22:06:41 +0800

    新建商品信息实体

---
+++ api/src/main/java/com/imooc/spring/entity/ProductInfo.java
@@ -0,0 +1,43 @@
+package com.imooc.spring.entity;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.Entity;
+import java.math.BigDecimal;
+import java.util.Calendar;
+
+/**
+ * @author zhangxishuo on 2018/4/20
+ * 商品详情实体
+ */
+
+@Entity
+@Data
+@NoArgsConstructor
+public class ProductInfo {
+
+    private String id;           // 商品id
+
+    private String name;         // 商品名称
+
+    private BigDecimal price;    // 单价
+
+    private Integer stock;       // 库存
+
+    private String description;  // 描述
+
+    private String icon;         // 图片
+
+    private Integer status;      // 状态
+
+    private Integer categoryType;  // 类目编号
+
+    @CreationTimestamp
+    private Calendar createTime;  // 创建时间
+
+    @UpdateTimestamp
+    private Calendar updateTime;  // 更新时间
+}
